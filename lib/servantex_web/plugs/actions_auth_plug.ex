c3bdbadbeed04e2fa7d26a77ef97e9bac6f3274e
Alisina Bahadori <alisina.bm@gmail.com>
2018-12-07 22:12:46 +0330

    Fix service token auth plug

--- lib/servantex_web/plugs/actions_auth_plug.ex
+++ lib/servantex_web/plugs/actions_auth_plug.ex
@@ -13,9 +13,10 @@ defmodule ServantexWeb.Plugs.ActionsAuthPlug do
 
   def init(opts), do: opts
 
-  def call(conn, opts) do
+  def call(conn, _opts) do
     with [auth_data | _] <- get_req_header(conn, "authorization"),
-         {:ok, user_id, token_id} <- Accounts.verify_token() do
+         [user_id, token] <- String.split(auth_data, ":"),
+         {:ok, user_id, token_id} <- Accounts.verify_service_token(user_id, token) do
       conn
       |> put_private(@auth_conn_priv_key, %{user_id: user_id, token_id: token_id})
     else
